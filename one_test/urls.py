5e9430e8ce8198e1a1bca8888721d47c80313d83
Sergey Dmitrievich <serj_1313@mail.ru>
2023-03-06 20:11:23 +0300

    Добавлен базовый костыльный функционал

--- one_test/urls.py
+++ one_test/urls.py
@@ -3,5 +3,7 @@ from .views import *
 
 
 urlpatterns = [
-    path('', home, name='home')
+    path('', home, name='home'),
+    path('test_code', test_code, name='test_code'),
+    path('get_status', get_status, name='get_status'),
 ]
--- one_test/views.py
+++ one_test/views.py
@@ -1,5 +1,31 @@
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 from django.shortcuts import render
+import subprocess
 
-
+@csrf_exempt
 def home(request):
     return render(request, 'one_test/home.html')
+
+def test_code(request, status={'answer': '', 'done': 1}):
+    if request == None:
+        next_out = open('stdout', 'r').read()
+        if next_out:
+            status['answer'] = next_out[next_out.find('\n'):]
+        open('stdout', 'w').truncate()
+        return status
+    data = request.GET.dict()
+    if request.method == 'GET' and 'code' in data and status['done'] == 1:
+        status['done'] = 0
+
+        open('stdout', 'w').truncate()
+        open('a.py', 'w').write(data['code'])
+        subprocess.call(['cf', 'submit', '-f', 'a.py', 'https://codeforces.com/contest/1186/problem/A'],
+                        stdout=open('stdout', 'w'),
+                        encoding='utf8')
+        
+        status['done'] = 1
+    return JsonResponse({}, status=200)
+
+def get_status(request):
+    return JsonResponse(test_code(None), status=200)
